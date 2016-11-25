e2d0e1a2b1bcf04e046542d6eb0201ddf59ae6ce
Kai Hilton-Jones <kaihj@users.noreply.github.com>
2016-11-25 11:48:33 +0000

    Update .probot.js

--- .probot.js
+++ .probot.js
@@ -1,3 +1,2 @@
 on('issue_comment.opened')
-  //.filter((event) => event.issue.body.match(/^Can I have some feedback$/))
-  .comment(':+1: Looks great! :shipit:')
+  .comment(':+1: Looks great! :shipit:');
