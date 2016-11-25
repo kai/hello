fb1c1b5b8bec24670a64065325adbf8f221fcd9b
Kai Hilton-Jones <kaihj@users.noreply.github.com>
2016-11-25 14:55:23 +0000

    Update .probot.js

--- .probot.js
+++ .probot.js
@@ -5,3 +5,11 @@ on('issues.opened', 'pull_request.opened')
 on('issues.labeled')
   .filter(event => event.payload.label.name == 'enhancement')
   .comment('Hey there, the `enhancement` label was added')
+
+on('issue_comment.opened')
+  .filter((event) => event.issue.body.match(/^@peter-robot assign @(\w+)$/))
+  .assign({{ matches[0] }});
+
+on('issue_comment.opened')
+  .filter((event) => event.issue.body.match(/^@peter-robot label @(\w+)$/))
+  .label($1);
