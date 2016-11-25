61004a1776560503dc9037d5005457cd0a27d06f
Kai Hilton-Jones <kaihj@users.noreply.github.com>
2016-11-25 11:39:28 +0000

    Update .probot.js

--- .probot.js
+++ .probot.js
@@ -1,3 +1,11 @@
 // Auto-respond to new issues and pull requests
-on('issues.opened', 'pull_request.opened')
-  .comment('Thanks for your comment')
+//on('issues.opened', 'pull_request.opened')
+//  .comment('Thanks for your comment')
+
+on('issue_comment.opened')
+  .filter((event) => event.issue.body.match(/Can I have some feedback, please?/))
+  .comment(':+1: Looks great! :shipit:')
+
+//on('issue_comment.opened')
+//  .filter((event) => event.issue.body.match(/^@probot label @(\w+)$/))
+//  .label($1);
