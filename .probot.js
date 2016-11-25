5fdffb19aca40159480847b7f3328547453cd425
Kai Hilton-Jones <kaihj@users.noreply.github.com>
2016-11-25 14:49:25 +0000

    Update .probot.js

--- .probot.js
+++ .probot.js
@@ -1,13 +1,10 @@
-on('issue_comment.opened')
-  .comment(':+1: Looks great! :shipit:')
-
 on('issues.opened', 'pull_request.opened')
   .comment('Thanks for your contribution!')
-  .label('test')
+  .label('test');
 
 on('issues.labeled')
   .filter(event => event.payload.label.name == 'enhancement')
-  .comment('Hey there, the `enhancement` label was added')
+  .comment('Hey there, the `enhancement` label was added');
 
 on('pull_request.labeled')
   .filter((event) => event.labeled(bug))
